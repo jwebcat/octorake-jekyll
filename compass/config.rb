4e28d53c751a29c4f075bba4b2c3828563913a73
jwebcat <jwebcat@gmail.com>
2013-03-24 01:14:47 -0700

    mv all assets to assets folder

---
+++ compass/config.rb
@@ -0,0 +1,15 @@
+# Compass configuration file.
+
+# Please read the docs http://docs.mixture.io/preprocessors#compass, we are working on support for framworks / plugins
+
+project_path = "/Users/Owner/Desktop/repc4t/jc4t"
+
+# Important! change the paths below to match your project setup
+css_dir = "public/styles" # update to the path of your css files.
+sass_dir = "public/styles/sass" # update to the path of your sass files.
+images_dir = "public/images" # update to the path of your image files.
+javascripts_dir = "public/scripts" # update to the path of your script files.
+
+line_comments = false # if debugging (using chrome extension - set this to true)
+cache = true
+color_output = false # required for Mixture
